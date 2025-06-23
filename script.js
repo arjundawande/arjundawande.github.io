1c2fa32b0efae04fa098a8402b1be7c022e45446
arjundawande <arjundawande16@gmail.com>
2025-06-23 17:18:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.onload = () =>{
   const test = document.getElementById('test');
   setInterval(()=>{
-      test.textContent = test.textContent + '.';
+    test.textContent = test.textContent + '.';
+    if(test.textContent == "Testing..."){
+      test.textContent = "Testing";
+    }
   },1000);
+  
 }
